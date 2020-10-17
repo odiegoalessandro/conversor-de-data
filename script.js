b1ad338a0ad382d46e31513d67e20ac774dfe481
odiegoalessandro <diegoalessandrodacruzmartins@gmail.com>
2020-10-17 19:19:59 -0300

    correcao de bugs

--- script.js
+++ script.js
@@ -5,36 +5,49 @@ var screen_result = document.getElementById('result')
 var btn = document.getElementById('calc')
 
 btn.addEventListener('click', () => {
+
     screen_result.value = ""
     let k = mountday.value + 2 * mountday.value + [3 * (mount.value + 1) / 5] + year.value + year.value / 4 - year.value / 100 + year.value / 400 + 2
     let result = (k % 7).toFixed(0)
     let weekday = "d"
-    if(result == 1){
-        weekday = 'Sabado'
-        return screen_result.innerText = weekday
+    if(mountday.value <= 31 && mount.value <= 12){
+        mount.value = ""
+        mountday.value = ""
+        year.value = ""
+        if(result == 1){
+            weekday = 'Sabado'
+            return screen_result.innerText = weekday
+        }
+        if(result == 2){
+            weekday = 'Domingo'
+            return screen_result.innerText = weekday
+        }
+        if(result == 3){
+            weekday = 'Segunda-feira'
+            return screen_result.innerText = weekday
+        }
+        if(result == 4){
+            weekday = 'Terça-feira'
+            return screen_result.innerText = weekday
+        }
+        if(result == 5){
+            weekday = 'Quarta-feira'
+            return screen_result.innerText = weekday
+        }
+        if(result == 6){
+            weekday = 'Quinta-feira'
+            return screen_result.innerText = weekday
+        }
+        if(result == 7){
+            weekday = 'Sexta-feira'
+            return screen_result.innerText = weekday
+        }
+  
     }
-    if(result == 2){
-        weekday = 'Domingo'
-        return screen_result.innerText = weekday
-    }
-    if(result == 3){
-        weekday = 'Segunda-feira'
-        return screen_result.innerText = weekday
-    }
-    if(result == 4){
-        weekday = 'Terça-feira'
-        return screen_result.innerText = weekday
-    }
-    if(result == 5){
-        weekday = 'Quarta-feira'
-        return screen_result.innerText = weekday
-    }
-    if(result == 6){
-        weekday = 'Quinta-feira'
-        return screen_result.innerText = weekday
-    }
-    if(result == 7){
-        weekday = 'Sexta-feira'
-        return screen_result.innerText = weekday
+    else{
+        mount.value = ""
+        mountday.value = ""
+        year.value = ""
+        alert('Preencha corretamente')
     }
 })
